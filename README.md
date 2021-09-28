f4511561151b365cc88c3abdd5ff2d8665e3fae0
fareska <fares.xb@gmail.com>
2021-09-28 22:01:09 +0300

    added read me file

---
+++ README.md
@@ -0,0 +1,26 @@
+This project is a football game service which through users can get list of matches (and games results)
+
+In the project directory, you can run:
+node server - you can define the port number which the server going to listen to in an env file otherwise the default prot is 5000.
+
+this project build with nodejs avoiding using any framework.
+the project loads football games data from two csv files and structure it in separated classes  
+
+The available endpoints:
+(assuming the user knows the teams and tournaments)
+
+GET:(http://localhost:5000)
+
+/matches/team/:team_name_
+/matches/tournament/:tournament_name
+
+
+/matches/team?name={the team name}&status={match status}
+the name parameter is required (in the upcoming version this will be fixed to /matches/team/name/status?)
+and the status parameter is optional 
+
+    for example the following get req will return all the matches played by Arsenal (the teams, result and so on..): 
+    /matches/team?name=arsenal&status=played
+    
+    another example, the following returns all the upcoming matches in the fa tournament.
+    /matches/tournament?name=fa&status=upcoming
--- server.js
+++ server.js
@@ -1,23 +1,36 @@
+// Import built-in Nodejs HTTP and URL modules  
 const http = require('http')
 const url = require('url')
+
 const Api = require('./dist/routes/api')
 const DataManager = require('./dist/data/DataManager')
 const DataLoader = require('./dist/data/DataLoader')
-
 const DataTable = require('./dist/data/classes/dataClasses')
 const MatchesTable = require('./dist/data/classes/matchesClass')
 
+//Instantiate a Matches class - for setting and retrieving matches data
 const matchesTable = new MatchesTable()
+
+//Instantiate a DataTable class -  class for saving array of items  
 const teamsTable = new DataTable()
 const tournamentsTable = new DataTable()
 
+//Instantiate a DataManager class -  class for serving the data    
 const dataManager = new DataManager(teamsTable, tournamentsTable, matchesTable)
+
+//Instantiate a DataLoader class -  class for loading data from csv files to a data manager     
 const dataLoader = new DataLoader(dataManager)
+
+//calling insertFilesDataToDataManager method and give it the csv file path as an argument.
 dataLoader.insertFilesDataToDataManager("./dist/data/dataOrigin/result_played.csv")
 dataLoader.insertFilesDataToDataManager("./dist/data/dataOrigin/result_upcoming.csv")
 
+//Instantiate an API class -  class for handling the "matches" routes requests and calling the appropriate methods from the relevant data manager      
 const api = new Api('matches', dataManager)
 
+// a function receives url address and request parameters, if they valid then it call a getData method from the appropriate api (otherwise it returns null)
+//(the matches api in pur case) and pass it it's parameters and an argument 
+// the getData method return the relevant data depending on the url and the parameters 
 const handleDataRouting = (pathName, params) => {
     let data = null
     if(params.name){
@@ -31,6 +44,7 @@ const handleDataRouting = (pathName, params) => {
     return data
 }
 
+// Create an http server object (a request Listener,) 
 const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname
     const params = url.parse(req.url, true).query
@@ -50,6 +64,7 @@ const server = http.createServer((req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
+//The http server object listen method - this method receives a port number and callback function.
 server.listen(PORT, () => console.log(`Server is running on port${PORT}`))
 
 
